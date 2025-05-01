a5a75511cb9182c3c99b349b7f0a3b439556d616
Danish  Pathan <112483716+DanishPathan12@users.noreply.github.com>
2025-05-02 01:47:38 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://social-media-xrhj.onrender.com",
+  baseURL: "http://34.133.240.60/",
   headers: { "Content-Type": "application/json" },
 
 });
