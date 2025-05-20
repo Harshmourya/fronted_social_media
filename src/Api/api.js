ab8bc543650ad506e1a4e20b47c717922c6832cf
Danish  Pathan <112483716+DanishPathan12@users.noreply.github.com>
2025-05-21 01:55:07 +0530

    Update api.js

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost/",
+  baseURL: "http://localhost:3000/",
   headers: { "Content-Type": "application/json" },
 
 });
